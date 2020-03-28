6365b0a0464ffefe55df772a6b08c9eafe796a0f
Vlad Movileanu <vladislav.movileanu@ati.utm.md>
2020-03-28 23:06:17 +0200

    master/ Created Project

---
+++ lab.2/imap.swift
@@ -0,0 +1,66 @@
+import UIKit
+//
+//  Get_Mails.swift
+//  Gmail-MUA
+//
+//  Created by vladikkk on 28/03/2020.
+//  Copyright © 2020 PR. All rights reserved.
+//
+
+import Foundation
+
+/*
+ A Singleton for extracting Inbox E-Mails.
+ */
+class Get_Inbox {
+    
+    // MARK: Properties
+    
+    // Login Info
+    private var hostname = "imap.gmail.com"
+    private var username = "el.zorro.show@gmail.com"
+    private var password = "87897631rvsn5"
+    
+    // Singleton
+    static let shared = Get_Inbox()
+    
+    // Session
+    var imapSession: MCOIMAPSession = MCOIMAPSession()
+    
+    // Configure Session Details
+    private init() {
+        imapSession.hostname = self.hostname
+        imapSession.username = self.username
+        imapSession.password = self.password
+        
+        imapSession.port = 993
+        imapSession.authType = MCOAuthType.saslPlain
+        imapSession.connectionType = MCOConnectionType.TLS
+        imapSession.isCheckCertificateEnabled = false
+    }
+    
+    // MARK: Methods
+    
+    func fetchMails()  {
+        
+        let requestKind: MCOIMAPMessagesRequestKind = .headers
+        let folder = "INBOX"
+        let uids = MCOIndexSet(range: MCORangeMake(1, UINT64_MAX))
+        
+        let fetchOperation = imapSession.fetchMessagesOperation(withFolder: folder, requestKind: requestKind, uids: uids)
+        
+        fetchOperation?.start({ error, fetchedMessages, vanishedMessages in
+            //Check for an error:
+            if error != nil {
+                if let error = error {
+                    print("Error downloading message headers:\(error)")
+                }
+            }
+            
+            //Print E-Mails
+            if let fetchedMessages = fetchedMessages {
+                print("The post man delivereth:\(fetchedMessages)")
+            }
+        })
+    }
+}
---
+++ lab.2/smtp.swift
@@ -0,0 +1,74 @@
+import UIKit
+//
+//  Send_Mail.swift
+//  Gmail-MUA
+//
+//  Created by vladikkk on 28/03/2020.
+//  Copyright © 2020 PR. All rights reserved.
+//
+
+import Foundation
+
+/*
+    A Singleton for sending a new E-Mail.
+*/
+class Send_Mail {
+    
+// MARK: Properties
+    
+    // Login Info
+    private var hostname = "smtp.gmail.com"
+    private var username = "el.zorro.show@gmail.com"
+    private var password = "87897631rvsn5"
+    
+    // Singleton
+    static let shared = Send_Mail()
+    
+    // Session
+    var smtpSession = MCOSMTPSession()
+    
+    // Configure Session Details
+    private init() {
+        smtpSession.hostname = self.hostname
+        smtpSession.username = self.username
+        smtpSession.password = self.password
+
+        smtpSession.port = 587
+        smtpSession.authType = MCOAuthType.saslPlain
+        smtpSession.connectionType = MCOConnectionType.startTLS
+        smtpSession.isCheckCertificateEnabled = false
+    }
+    
+// MARK: Methods
+
+    // Send New Mail using SMTP-Session
+    func send() {
+
+        print(#function)
+
+        smtpSession.connectionLogger = {(connectionID, type, data) in
+            if data != nil {
+                if let string = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
+                    NSLog("Connectionlogger: \(string)")
+                }
+            }
+        }
+
+        let builder = MCOMessageBuilder()
+        builder.header.to = [MCOAddress(displayName: "zorro", mailbox: "el.zorro.show@gmail.com")!]
+        builder.header.from = MCOAddress(displayName: "me", mailbox: "el.zorro.show@gmail.com")
+        builder.header.subject = "TEST01"
+        builder.htmlBody = "This is a test message!"
+
+        let rfc822Data = builder.data()
+        let sendOperation = smtpSession.sendOperation(with: rfc822Data)
+        sendOperation?.start { (error) -> Void in
+            if (error != nil) {
+                NSLog("Error sending email: \(String(describing: error))")
+            } else {
+                NSLog("Successfully sent email!")
+            }
+        }
+    }
+    
+}
