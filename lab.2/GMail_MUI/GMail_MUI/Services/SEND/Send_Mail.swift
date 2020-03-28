db177bbad393a104b5afc8a9d56eea806bcb6d1b
Vlad <36297828+vladikkk23@users.noreply.github.com>
2020-03-29 00:28:20 +0200

    Update Send_Mail.swift

--- lab.2/GMail_MUI/GMail_MUI/Services/SEND/Send_Mail.swift
+++ lab.2/GMail_MUI/GMail_MUI/Services/SEND/Send_Mail.swift
@@ -17,8 +17,8 @@ class Send_Mail {
     
     // Login Info
     private var hostname = "smtp.gmail.com"
-    private var username = "el.zorro.show@gmail.com"
-    private var password = "87897631rvsn5"
+    private var username = "Your Gmail Username"
+    private var password = "Your Password"
     
     // Singleton
     static let shared = Send_Mail()
