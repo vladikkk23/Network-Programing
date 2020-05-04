5c125a958cdc1ffaec6e7850fb765a052bbd162e
Vlad Movileanu <vladislav.movileanu@ati.utm.md>
2020-05-04 15:41:40 +0300

    Development/ Project init.

---
+++ lab.4/ChatRoomwithTCPSocket/ChatRoomwithTCPSocket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatRoomwithTCPSocket
+//
+//  Created by vladikkk on 04/05/2020.
+//  Copyright © 2020 PR. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
